6eaf9e6a32c9491aa54a9089194c490e28e31ac2
ofdun <elik394@mail.ru>
2023-08-17 18:44:03 +0300

    help info added

--- const.py
+++ const.py
@@ -3,4 +3,6 @@ CURRENT_PATH = 'E:\Elisey\.Projects\discordCongratulator' # Modify for your own
 HELP = """
 Я могу поздравить тебя с праздиником!
 /gz - Поздравить открыткой.
+/settime - Установить рассылку на определенное время ( например 07:00 )
+/removemailing - Отключить рассылку
 """
